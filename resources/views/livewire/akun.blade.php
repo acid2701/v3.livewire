89a20c55cca8a38b260d3f1e5eb837d7784523d8
acid2701 <caturhasbi@gmail.com>
2025-05-10 23:38:42 +0700

    install livewire + tilwindcss +daisyui

---
+++ resources/views/livewire/akun.blade.php
@@ -0,0 +1,5 @@
+<div>
+  <div class="bg-blue-500 text-white p-4">
+  Tailwind berhasil diinstall!
+</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\Akun;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',Home::class)->name('home');
+Route::get('/akun',Akun::class)->name('akun');
